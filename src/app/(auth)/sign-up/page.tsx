a3d54d30dc0e2ddb3f353c72814a8dec3a4393c2
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2025-02-15 22:47:54 +0600

    authlayout create and env file setup

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,4 @@
+import AuthForm from "@/components/AuthForm";
+
+const signUp = () => <AuthForm type="sign-up" />;
+export default signUp;
---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,153 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+
+const AuthForm = ({ type }: { type: "sign-in" | "sign-up" }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const formSchema = z.object({
+    fullName:
+      type === "sign-up"
+        ? z
+            .string()
+            .min(3, { message: "Full name must be at least 3 characters long" })
+            .max(40, { message: "Full name cannot exceed 40 characters" })
+            .regex(/^[a-zA-Z\s]+$/, {
+              message: "Full name can only contain letters and spaces",
+            })
+            .refine((val) => !/[<>$%{}]/.test(val), {
+              message: "Invalid characters detected",
+            }) // Prevents special characters used in injections
+        : z.string().optional(),
+
+    email: z
+      .string()
+      .trim()
+      .email({ message: "Invalid email format" })
+      .min(8, { message: "Email must be at least 8 characters long" })
+      .max(80, { message: "Email cannot exceed 80 characters" })
+      .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+        message: "Invalid email format",
+      }) // Stricter validation
+      .refine((val) => !/[<>$%{}]/.test(val), {
+        message: "Invalid characters detected",
+      }), // Prevents XSS attacks
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+          {errorMessage && <p className="error-message">*{errorMessage}</p>}
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="ml-1 font-medium text-brand"
+            >
+              {" "}
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default AuthForm;
